720ee7df4e5b590104c545c69108172b97c1b4b2
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-11-03 21:36:40 +0400

    api services

---
+++ src/services/apiServices.js
@@ -0,0 +1,38 @@
+const API_URL = 'https://localhost:7040/api/Publisher'
+
+export const postPublisher = async (data) => {
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deletePublisher = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
